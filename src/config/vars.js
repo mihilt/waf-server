d5356c0071718d409a8fdb9e59bec06dbfefcb5a
mihilt <kwschic@gmail.com>
2023-02-02 00:29:47 +0900

    feat: add aes256's encrypt and decrypt function - also add key and handle it in .env file

--- src/config/vars.js
+++ src/config/vars.js
@@ -2,6 +2,7 @@ module.exports = {
   env: process.env.NODE_ENV,
   serverPort: process.env.SERVER_PORT,
   mongoUri: process.env.MONGO_URI,
+  aes256Key: process.env.AES256_KEY,
   gmailID: process.env.GMAIL_ID,
   gmailPassword: process.env.GMAIL_PASSWORD,
   gmailName: process.env.GMAIL_NAME,
---
+++ src/utils/aes256.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+const { aes256Key } = require('../config/vars');
+
+const algorithm = 'aes-256-cbc';
+const key = aes256Key;
+
+exports.encrypt = text => {
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return `${iv.toString('hex')}:${encrypted.toString('hex')}`;
+};
+
+exports.decrypt = text => {
+  const textParts = text.split(':');
+  const iv = Buffer.from(textParts.shift(), 'hex');
+  const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+};
