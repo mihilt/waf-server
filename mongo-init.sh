32a611fc672c6d810e97e7bc59a272ec2c5c7bc7
mihilt <kwschic@gmail.com>
2023-02-05 00:08:41 +0900

    chore: fix to use mongosh in mongodb container
    - now ubuntu don't need to install mongodb

--- mongo-init.sh
+++ mongo-init.sh
@@ -9,7 +9,7 @@ sleep 10
 
 # create mongo user
 echo "=> Create mongo user..."
-mongosh --host localhost --port 27017 -u "${MONGO_INITDB_ROOT_USERNAME}" -p "${MONGO_INITDB_ROOT_PASSWORD}" \
+docker exec waf-db mongosh --host localhost -u "${MONGO_INITDB_ROOT_USERNAME}" -p "${MONGO_INITDB_ROOT_PASSWORD}" \
   --eval "use ${MONGO_INITDB_DATABASE}" \
   --eval "db.init.insertOne({result: true})" \
   --eval "db.createUser({ user: '${MONGO_WAF_USERNAME}', pwd: '${MONGO_WAF_PASSWORD}', roles: [ { role: 'readWrite', db: '${MONGO_INITDB_DATABASE}' } ] })"
