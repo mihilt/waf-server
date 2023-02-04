b1608534857cc1059979778060fe28b978b9f384
mihilt <kwschic@gmail.com>
2023-02-04 20:56:51 +0900

    chore: add volume in express for file upload

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,11 @@
 # git pull
 echo "=> Git pull..."
 git pull https://${GITHUB_ID}:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}.git
+
+# down docker-compose
+echo "=> Down docker-compose..."
+docker-compose down -v
+
+# build and up docker-compose
+echo "=> Build and up docker-compose..."
+docker-compose up -d --build
